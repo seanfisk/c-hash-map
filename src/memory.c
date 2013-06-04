455a5ff3adda5b28c1027172ea16e053840f5e39
softboysxp <softboysxp@gmail.com>
2013-06-03 20:02:41 -0600

    fixed: safe_malloc did not return

--- src/memory.c
+++ src/memory.c
@@ -5,12 +5,16 @@
 
 void *safe_malloc(size_t size) {
 	void *ptr = malloc(size);
+
 	if(!ptr) {
 		fputs("out of memory", stderr);
 		exit(1);
 	}
+
+	return ptr;
 }
 
 void safe_free(void *ptr ) {
 	free(ptr);
 }
+
