92645ac0f89e581de91b4f76ddd7b03f94ffba22
Babak Behzad <babakbehzad@gmail.com>
2013-06-17 21:58:27 -0500

    add hash-functions feature to support different hash functions

---
+++ src/hash_functions.h
@@ -0,0 +1,11 @@
+/**
+ * @file
+ * Implementation of different hash functions.
+ */
+
+#ifndef HASH_FUNCTIONS_H
+#define HASH_FUNCTIONS_H
+
+size_t hash_map_default_hash_func(const void *key, size_t capacity);
+
+#endif /*HASH_FUNCTIONS_H*/
